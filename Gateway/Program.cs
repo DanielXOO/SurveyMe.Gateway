773cd6ceda177b79c9371f1414fe90c3d04b6097
DanielXOO <dankulinkovich@gmail.com>
2022-08-04 10:25:21 +0300

    Setup CORS

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -6,12 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin());
 
 await app.UseOcelot();
 
